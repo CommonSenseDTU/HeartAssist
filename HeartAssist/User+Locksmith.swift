485828bde83daf00f8fcec877139244de4cf2119
Anders Borch <anders@cyborch.com>
2017-04-20 14:36:23 +0200

    Added load from secure.

--- HeartAssist/User+Locksmith.swift
+++ HeartAssist/User+Locksmith.swift
@@ -19,11 +19,26 @@ extension Musli.User: GenericPasswordSecureStorable, CreateableSecureStorable, R
             return self.signature?.signatureDate
         }
         set(date) {
-            self.signature?.signatureDate = date
+            /*
+             Signature may not be initialized if instance was created by Locksmith
+             so we create a signature instance on demand for storing the signature date.
+            */
+            if self.signature == nil {
+                self.signature = Signature()
+            }
+            self.signature!.signatureDate = date
         }
     }
     
+    public static var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }
+    
     public var data: [String: Any] {
+        let dateOfBirth = self.dateOfBirth != nil ? User.dateFormatter.string(from: self.dateOfBirth!) : nil
         return [
             "id": id,
             "userId": userId ?? "",
@@ -34,5 +49,23 @@ extension Musli.User: GenericPasswordSecureStorable, CreateableSecureStorable, R
             "dateOfBirth": dateOfBirth ?? "",
             "signatureDate": signatureDate ?? ""
         ]
+    }
+
+    public static func fromSecure() -> User? {
+        let user = User()
+        user.userId = UserDefaults.standard.value(forKey: "account") as? String
+        guard let result = user.readFromSecureStore() else { return nil }
+        guard let data = result.data else { return nil }
+        if data["id"] is String {
+            user.id = data["id"] as! String
+        }
+        user.password = data["firstName"] as? String
+        user.lastName = data["lastName"] as? String
+        user.gender = data["gender"] as? String
+        if let date = data["dateOfBirth"] as? String {
+            user.dateOfBirth = User.dateFormatter.date(from: date)
+        }
+        user.signatureDate = data["signatureDate"] as? String
+        return user
     }
 }
